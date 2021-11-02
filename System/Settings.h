cbf56651a5a53b0fd34350037d859136a74e93a2
KayleePosthumus <kayleeposthumus1@gmail.com>
2021-11-02 13:44:05 +0200

    Added initial rocket configurations.

--- System/Settings.h
+++ System/Settings.h
@@ -1,7 +1,34 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-class Settings
+struct Falcon9Config
+{
+    static const int Falcon9CoresCount = 1;
+    static const int MerlinEnginesCount = 9;
+    static const int VacuumEnginesCount = 1;
+    static const int SatelliteLimit = 60;
+};
+
+struct FalconHeavyConfig
+{
+    static const int Falcon9CoresCount = 3;
+    static const int MerlinEnginesCount = 27;
+    static const int VacuumEnginesCount = 1;
+};
+
+struct DragonConfig
+{
+    static const bool CanCarryCargo = 1;
+    static const bool CanCarryCrew = 0;
+};
+
+struct CrewDragonConfig
+{
+    static const bool CanCarryCargo = 1;
+    static const bool CanCarryCrew = 0;
+};
+
+class RNG
 {
 public:
     static const float EngineDamageLiftOffProbability = 0.25;
