bc6f2c16826370ae7d5ecdd0b6ba8849fccb9adc
Fontacorn <fontacorn@gmail.com>
2021-11-19 15:36:04 +0200

    Asses basic iterator and mediator

--- System/StarlinkSatellite/StarlinkSatellite.h
+++ System/StarlinkSatellite/StarlinkSatellite.h
@@ -1,12 +1,25 @@
-#ifndef STARLINKSATELITE_H
-#define STARLINKSATELITE_H
+#ifndef STARLINKSATELLITE_H
+#define STARLINKSATELLITE_H
+
+#include <string>
+#include <cstdio>
+#include "Iterator.h"
 
 class StarlinkSatellite
 {
 private:
+	std::string name;
+	StarlinkSatellite* next;
 public:
 	StarlinkSatellite();
+	StarlinkSatellite(std::string);
 	~StarlinkSatellite();
+	StarlinkSatellite* clone(StarlinkSatellite*);
+	StarlinkSatellite* getNext();
+	void communicateSatellite();
+	void communicateGround();
+	Iterator* createIterator();
+	std::string getName();
 };
 
 #endif
