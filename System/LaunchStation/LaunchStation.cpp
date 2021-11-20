59ee5547a7e9d21b0583a87bac99bebffe14d59f
Fontacorn <fontacorn@gmail.com>
2021-11-20 16:24:52 +0200

    Added basic launch station

--- System/LaunchStation/LaunchStation.cpp
+++ System/LaunchStation/LaunchStation.cpp
@@ -1,4 +1,20 @@
 #include "LaunchStation.h"
 
 LaunchStation::LaunchStation() {}
-LaunchStation::~LaunchStation() {}
+
+LaunchStation::~LaunchStation() {
+	delete LaunchStation::launchStation;
+}
+
+LaunchStation* LaunchStation::instance()
+{
+	if (!LaunchStation::launchStation)
+		launchStation = new LaunchStation();
+
+	return launchStation;
+}
+
+void LaunchStation::setSatelliteList(StarlinkSatellite* satelliteList)
+{
+	LaunchStation::launchStation->satelliteList = satelliteList;
+}
