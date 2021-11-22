d6e685726e897b7362f233aab373b9d97e31b2c8
ListlessPanda <84448921+ListlessPanda@users.noreply.github.com>
2021-11-22 11:29:04 +0200

    observer handle

--- System/Stage/Stage1.cpp
+++ System/Stage/Stage1.cpp
@@ -1,11 +1,12 @@
 #include "Stage1.h"
-
+#include <iostream>
+using namespace std;
 Stage1::Stage1() {}
 Stage1::~Stage1() {}
 
 void Stage1::handle() 
 {
-    
+    cout << "Rocket in stage 1" << endl;
 }
 
 Stage* Stage1::Clone()
